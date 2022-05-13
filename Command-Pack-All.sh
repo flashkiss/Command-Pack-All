a83d88137a21976ba1294f19ebc7090f00de18eb
R14A <78781611+ITS-R14A@users.noreply.github.com>
2022-05-13 13:46:25 +0600

    Create Command-Pack-All.sh

---
+++ Command-Pack-All.sh
@@ -0,0 +1,103 @@
+#!/data/data/com.termux/files/usr/bash
+
+echo $red
+
+echo -e "\e[1;32m——————————————————————————————————————————\e[0m"
+
+echo -e "\e[1;31m ########::::::##::::##:::::::::::'###::::\e[0m"
+
+echo -e "\e[1;31m ##.... ##:::####::: ##::::##::::'## ##:::\e[0m"
+
+echo -e "\e[1;31m ##:::: ##::.. ##::: ##::: ##:::'##:. ##::\e[0m"
+
+echo -e "\e[1;31m ########::::: ##::: ##::: ##::'##:::. ##:\e[0m"
+
+echo -e "\e[1;31m ##.. ##:::::: ##::: #########: #########:\e[0m"
+
+echo -e "\e[1;31m ##::. ##::::: ##:::...... ##:: ##.... ##:\e[0m"
+
+echo -e "\e[1;31m ##:::. ##::'######::::::: ##:: ##:::: ##:\e[0m"
+
+echo -e "\e[1;31m..:::::..:::......::::::::..:::..:::::..::\e[0m"
+
+echo -e "\e[1;32m——————————————————————————————————————————\e[0m"
+
+echo -e "\e[1;32m   Tool Author: SHIMANTO ISLAM (R14A)     \e[0m"
+
+echo -e "\e[1;32m  Contract: Shimantoislam066@gmail.com    \e[0m"
+
+echo -e "\e[1;32m      (Don't Try to Edit My Code)         \e[0m"
+
+echo -e "\e[1;31m      (!!Must Need To Internet!!)         \e[0m"
+
+sleep 3
+
+apt update && upgrade  -y                      #update in progress
+
+apt install python -y           #installation......
+
+apt install python2 -y
+
+apt install ruby -y
+
+apt install git -y
+
+apt install curl -y
+
+apt install php -y
+
+apt install perl -y
+
+apt install nmap -y
+
+apt install bash -y
+
+apt install clang -y
+
+apt install jq -y
+
+apt install macchanger -y
+
+apt install nano -y
+
+apt install tar -y
+
+apt install zip -y
+
+apt install unzip -y
+
+apt install tor -y
+
+apt install wget -y
+
+apt install wcalc -y
+
+apt install openssl
+
+apt install bmon -y
+
+sleep 1
+
+echo -e "\e[1;32m YOUR PACKAGES IS INSTALLED SUCCESSFULLY!!\e[0m"
+
+sleep 4
+
+clear
+
+echo -e "\e[1;32m——————————————————————————————————————————\e[0m"
+
+echo -e "\e[1;32m               ★Thank You★                \e[0m"
+
+echo -e "\e[1;32m———————————————Use Our Tool——————————————\e[0m"
+
+echo -e "\e[1;32m‌‌‌!!This Tool Is Auto Remove Your Device❣!!\e[0m"
+
+echo -e "\e[1;32mYour Need Again This Tool Please Install Again!\e[0m"
+
+echo -e "\e[1;32m——————————————————————————————————————————\e[0m"
+
+sleep 4
+
+clear
+
+rm -rf Command-Pack-All.sh
